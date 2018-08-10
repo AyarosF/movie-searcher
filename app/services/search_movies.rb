557c9cb9deda27ddc62ce88dd95a57564ed2f08f
AyarosF <sorayafructuoso@gmail.com>
2018-08-10 02:28:33 +0200

    modif indent

--- app/services/search_movies.rb
+++ app/services/search_movies.rb
@@ -1,8 +1,8 @@
 class SearchMovies
-	def perform(title)
-      @search = Tmdb::Search.new
-      @search.resource('movie') # determines type of resource
-      @search.query(title) # the query to search against
-      @search.fetch # makes request
+  def perform(title)
+    @search = Tmdb::Search.new
+    @search.resource('movie') # determines type of resource
+    @search.query(title) # the query to search against
+    @search.fetch # makes request
   end
 end
