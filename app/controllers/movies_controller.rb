303cbdb3628e992512bf0d95230f0af33c6c666d
AyarosF <sorayafructuoso@gmail.com>
2018-08-10 02:35:25 +0200

    sleep pour eviter bug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,10 +7,10 @@ class MoviesController < ApplicationController
     @images = []
     @movies.each do |movie|
       @titles << movie['title']
-      sleep(0.5)
+      sleep(0.2)
       @releases << movie['release_date']
       @images << "http://image.tmdb.org/t/p/w185#{movie['poster_path']}"
-      sleep(0.5)
+      sleep(0.2)
       begin
         Tmdb::Movie.credits(movie['id'])['crew'].find {|x| x['job'] == 'Director'}['name'].nil? || Tmdb::Movie.credits(movie['id'])['crew'].find {|x| x['job'] == 'Director'}['name'].empty?
       rescue NoMethodError, StandardError => e
